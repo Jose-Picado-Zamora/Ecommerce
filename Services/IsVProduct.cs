4199af47256176b8700b72da60895a47c70d404a
Billy <billyugalde2004@gmail.com>
2024-05-03 17:15:06 -0600

    Interfaces shopping lista

--- Services/IsVProduct.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entities;
-
-namespace Services
-{
-    public interface IsVProduct
-    {
-        public Product AddProduct(Product product);
-    }
-}
---
+++ Services/Product/IsVProduct.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+
+namespace Services
+{
+    public interface IsVProduct
+    {
+        public Product AddProduct(Product product);
+        public Product DeleteProduct(Product product, int id);
+        public Product UpdateProduct(int id);
+        public List<Product> GetAllProduct();
+        public Product GetProductById(int id);
+    }
+}
+
