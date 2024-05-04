11bd54de56dad1f33587a8dc170157bdcf87c3ce
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2024-05-03 18:25:28 -0600

    agregue las interfaces user y bill

---
+++ Services/Bill/IsVBill.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+
+namespace Services
+{
+    public interface IsVBill
+    {
+        public Bill AddBill(Bill bill);
+        public Bill RemoveBill(int id);
+        public Bill UpdateBill(int id,Bill bill);
+        public List<Bill> GetAllBill();
+        public Bill GetBillById(int id);
+        public bool SendEmail();
+
+    }
+}
