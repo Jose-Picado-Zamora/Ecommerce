705a9a98a9499205f29986d90244be6283083246
Billy <billyugalde2004@gmail.com>
2024-05-03 18:13:29 -0600

    Actualizacion de entidades

---
+++ Services/ShoppingCart/ISvShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface ISvShoppingCart
+    {
+        public Product AddProduct(Product product);
+        public Product UpdateProduct(int id, Product product);
+        public void DeleteShoppingCart(int id);
+        public string BuyConfirmation();
+        public double Calculate();
+
+
+    }
+}
