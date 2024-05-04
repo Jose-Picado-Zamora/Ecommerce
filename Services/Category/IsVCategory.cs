a715673fc191c4809444951c3eeb904dfbb5f77c
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2024-05-03 23:35:15 -0600

    Añadi la interfaz de categoria

---
+++ Services/Category/IsVCategory.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace Services
+{
+    public interface IsVCategory
+    {
+        public Product AddProductToCategory(Product product);
+        public Product RemoveProductToCategory(int id);
+        List<Category> GetAllCategories();
+        public Category GetCategoryById(int id);
+    }
+}
