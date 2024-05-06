9173813e76760a55b295fc99a5d6ac8e7e9c84c4
Billy <billyugalde2004@gmail.com>
2024-05-06 00:18:55 -0600

    ...

--- Services/Bill/IsVBill.cs
+++ Services/Bill/IsVBill.cs
@@ -5,7 +5,7 @@ namespace Services
     public interface IsVBill
     {
         public Bill AddBill(Bill bill);
-        //
+        //....
         public Bill RemoveBill(int id);
         public Bill UpdateBill(int id,Bill bill);
         public List<Bill> GetAllBill();
