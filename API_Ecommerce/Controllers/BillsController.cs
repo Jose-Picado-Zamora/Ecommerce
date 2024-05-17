fa1d5577eaf9f146a1e4280f44fad4cc11dfaca4
sebastian <sebastian.campos.calvo@est.una.ac.cr>
2024-05-17 00:45:20 -0600

    BillsController

--- API_Ecommerce/Controllers/BillsController.cs
+++ API_Ecommerce/Controllers/BillsController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Bills;
 
 namespace API_Ecommerce.Controllers
 {
     public class BillsController : Controller
     {
-        public IActionResult Index()
+
+        private IsVBill _svBill;
+        public BillsController(IsVBill svBill)
         {
-            return View();
+            _svBill = svBill;
+        }
+
+      
+        [HttpGet]
+        public IEnumerable<Bill> Get()
+        {
+            return _svBill.GetAllBill();
+        }
+
+        
+        [HttpGet("{id}")]
+        public Bill Get(int id)
+        {
+            var result = _svBill.GetBillById(id);
+            return _svBill.GetBillById(id);
+        }
+
+      
+        [HttpPost]
+        public void Post([FromBody] Bill bill)
+        {
+            _svBill.AddBill(bill);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Bill bill)
+        {
+            _svBill.UpdateBill(id, new Bill
+            {
+                UserId = bill.UserId,
+                 datetime = bill.datetime,
+                 paymentMethod = bill.paymentMethod,
+
+           
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _svBill.RemoveBill(id);
         }
     }
 }
