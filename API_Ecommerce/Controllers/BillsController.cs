d4ce44309ef728ecfc9c9f7d4e45a205341b9db7
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2024-05-17 00:19:42 -0600

    Agregue BillsController

---
+++ API_Ecommerce/Controllers/BillsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Ecommerce.Controllers
+{
+    public class BillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
