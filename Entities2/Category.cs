4f0c758579a9fd0061aa7f320b2ca42af0d70131
Jose-Picado-Zamora <joseandrespicadozamora9@gmail.com>
2024-05-18 11:39:28 -0600

    arreglado

---
+++ Entities2/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Category
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<Product>? Products { get; set; }
+
+    }
+}
