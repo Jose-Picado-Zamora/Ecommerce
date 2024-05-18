b05cb915ace3f10d8e721d69a7be4e713a0bd02b
RobertoCampos <roberto.campos.calvo@est.una.ac.cr>
2024-05-17 18:04:03 -0600

    Correccion Category y Primera Configuracion Program

--- API_Ecommerce/Program.cs
+++ API_Ecommerce/Program.cs
@@ -1,8 +1,35 @@
+using Services;
+using Services.Bills;
+using Services.Products;
+using Services.Categories;
+using Services.Users;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IsVBill, SvBill>();
+builder.Services.AddScoped<ISvProduct, SvProduct>();
+builder.Services.AddScoped<IsVCategory, SvCategory>();
+builder.Services.AddScoped<IsVUser, SvUser>();
 
-builder.Services.AddControllers();
+
+//Manejo de ciclo infinito
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(x =>
+ x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Policy1", builder =>
+    {
+        builder.WithOrigins("https://localhost:5173")
+            .WithMethods("GET", "POST")
+            .WithHeaders("Content-Type");
+    });
+
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,7 +45,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseUserization();
+app.UseAuthorization();
 
 app.MapControllers();
 
