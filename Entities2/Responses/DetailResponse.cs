3112557564284a9d1b07677718528ea43fc85e52
RobertoCampos <roberto.campos.calvo@est.una.ac.cr>
2024-06-08 04:06:48 -0600

    Bill response

---
+++ Entities2/Responses/DetailResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class DetailResponse
+    {
+        public int quantity { get; set; }
+        public double? subtotal { get; set; } = 0;
+        public int ProductId { get; set; }
+        public ProductResponseDetail Product { get; set; }
+    }
+}
